b0c25f6d0f419a8dfd41e8c098908e841a43af3c
Ryan Castner <castner.rr@gmail.com>
2018-01-02 13:24:35 -0500

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='django-auditlog',
+    name='dj-rit-auditlog',
     version='0.4.4',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
-    url='https://github.com/jjkester/django-auditlog',
+    url='https://github.com/audiolion/dj-rit-auditlog',
     license='MIT',
-    author='Jan-Jelle Kester',
+    author='Jan-Jelle Kester, Ryan Castner',
     description='Audit log app for Django',
     install_requires=[
         'django-jsonfield>=1.0.0',
